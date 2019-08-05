6327dd997250215ff3815eef27424fae3876fb92
wys <wyscode@163.com>
2019-08-05 22:45:49 +0800

    chapter 12 exercise 28

--- ch12/README.md
+++ ch12/README.md
@@ -2,6 +2,14 @@
 
 ***
 
+#### Exercise 12.27
+
+[test](e27/testquery.cpp) | [TextQuery](e27/text_query.h) | [QueryResult](e27/query_result.h)
+
+#### Exercise 12.28
+
+[c12e28](e28/c12e28.cpp)
+
 #### Exercise 12.29
 
-[do while & while](ch12_27/testquery.cpp)
+[do while & while](e27/testquery.cpp)
--- ch12/e28/c12e28.cpp
+++ ch12/e28/c12e28.cpp
@@ -1,9 +1,54 @@
 #include <iostream>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
 
-using namespace std;
+using std::vector;
+using std::string;
+using std::map;
+using std::set;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::ifstream;
+using std::istringstream;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    vector<string> file;
+    map<string, set<int>> w_s_map;//word set map
+    map<string, int> w_c_map;//word count map
+    string search, file_name, line, word;
+    int n_line = 1;
+
+    cout << "Please input the file in which you want to query:" << endl;
+    cin >> file_name;
+    ifstream ifs(file_name);
+    while (getline(ifs, line))
+    {
+        file.push_back(line);
+        istringstream iss(line);
+        while (iss >> word)
+        {
+            //handle punctuations
+            w_s_map[word].insert(n_line);
+            ++w_c_map[word];
+        }
+        ++n_line;
+    }
+
+    while (true)
+    {
+        cout << "Please input the word to query or q to quit:" << endl;
+        if (!(cin >> search) || ("q" == search)) break;
+        cout << w_c_map[search] << endl;
+        for (auto m_iter = w_s_map[search].cbegin(); m_iter != w_s_map[search].cend(); ++m_iter)
+        {
+            cout << file[*m_iter - 1] << endl;
+        }
+    }
     return 0;
 }
