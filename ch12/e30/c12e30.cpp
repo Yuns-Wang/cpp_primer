d1a41b75d3b2cecff1aa5314ef5f7ff95311758d
wys <wyscode@163.com>
2019-08-05 22:54:49 +0800

    chapter 12 exercise 30 scratch

---
+++ ch12/e30/c12e30.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "text_query.h"
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
---
+++ ch12/e30/text_query.h
@@ -0,0 +1,9 @@
+#ifndef TEXT_QUERY_H
+#define TEXT_QUERY_H
+
+class TextQuery{
+public:
+private:
+};
+
+#endif // TEXT_QUERY_H
