1ae55b4be461b1c2803c13a6c03711e792c3d493
wys <wyscode@163.com>
2019-08-19 23:37:57 +0800

    chapter 13 exercise 14 15 16 17

---
+++ ch13/c13e15.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+class numberd {
+public:
+	numberd():mysn(++count) {}
+	numberd(const numberd&):mysn(++count) {}
+	friend void f(numberd);
+private:
+	static int count;
+	int mysn;
+};
+
+int numberd::count = 0;
+
+void f (numberd s) { std::cout << s.mysn << std::endl; }
+
+int main()
+{
+	numberd a, b = a, c = b;
+	f(a); f(b); f(c);
+}
