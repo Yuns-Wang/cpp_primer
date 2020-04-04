054198c6c3287858726e72214885d4bb6398b3c3
wys <wyscode@163.com>
2020-04-04 23:46:43 +0800

    chapter 13 exercise 19

---
+++ ch13/c13e19.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <iostream>
+
+using std::string;
+
+class Employee {
+public:
+    Employee() : id(s_id++) {}
+    Employee(const string &name):name(name), id(s_id++){}
+    Employee(const Employee &) = delete;
+    Employee &operator=(const Employee&) = delete;
+    void show() const {std::cout << name << id << s_id << std::endl;}
+
+private:
+    static int s_id;
+    string name;
+    int id;
+};
+
+int Employee::s_id = 0;
+
+int main()
+{
+    Employee e1;
+    Employee e2("wys");
+    e1.show();
+    e2.show();
+    return 0;
+}
